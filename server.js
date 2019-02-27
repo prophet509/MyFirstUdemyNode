383e7a210c119455691f04a8a6b3d1b802633322
prophet509 <31301393+prophet509@users.noreply.github.com>
2019-02-28 00:49:32 +0700

    adsfas
    FIrst

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+const  fs = require('fs');
+
+
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    var log = `${now}:${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log + '\n', (err) => {
+        if(err){
+            console.log(err);
+        }
+    });
+    next();
+});
+hbs.registerHelper('getCurrentYear',() => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text) => {
+    return text.toUpperCase();
+});
+// Get Homepage
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: "Home Page",
+        welcomeMessage: "Tram Bich"
+    });
+});
+app.use("/about", (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: "About Page",
+        welcomeMessage: "Some text here"
+
+    });
+})
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: "Unable to handle request"
+    });
+});
+app.listen(3000, () => {
+    console.log("Server is stating at port 3000");
+});
